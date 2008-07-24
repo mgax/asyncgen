2a2d19ed52260d3a8f168dd8347ea3dae7e6aec8
Alex Morega <alex@grep.ro>
2008-07-25 01:07:33 +0300

    async_with_input with one input source

--- asyncgen.py
+++ asyncgen.py
@@ -1,37 +1,90 @@
 import pprocess
 
-def _async_process(func, args, kwargs):
+def _async_process(func, args, kwargs, inputs):
     channel = pprocess.create()
     if channel.pid != 0:
         return channel
     
-    ret = func(*args, **kwargs)
     try:
-        for msg in ret:
-            channel.send(('next', msg))
+        for i in inputs:
+            i.set_channel(channel)
+        
+        gen = func(*args, **kwargs)
+        try:
+            for v in gen:
+                channel.send(('next', v))
+        except Exception, e:
+            channel.send(('exception', e))
+        
+        channel.send(('exception', StopIteration()))
+    
     except Exception, e:
         channel.send(('exception', e))
     
-    channel.send(('exception', StopIteration()))
-    
-    pprocess.exit(channel)
+    finally:
+        pprocess.exit(channel)
 
 class AsyncJob(object):
-    def __init__(self, func, args, kwargs):
-        self.channel = _async_process(func, args, kwargs)
+    def __init__(self, func, args, kwargs, async_kwargs=None):
+        if async_kwargs:
+            async_inputs = [AsyncInput()]
+            kwargs = dict(kwargs, **{async_kwargs.keys()[0]: async_inputs[0]})
+            self.input = async_kwargs.values()[0].__iter__()
+        else:
+            async_inputs = []
+            self.input = None
+        
+        self.channel = _async_process(func, args, kwargs, async_inputs)
     
     def __iter__(self):
         return self
     
     def next(self):
-        ty, val = self.channel.receive()
-        if ty == 'next':
-            return val
+        t, v = self.channel.receive()
+        
+        while t == 'pull':
+            try:
+                iv = self.input.next()
+                self.channel.send(('next', iv))
+            except Exception, ie:
+                self.channel.send(('exception', ie))
+            
+            t, v = self.channel.receive()
+        
+        if t == 'next':
+            return v
         else:
-            raise val
+            raise v
 
 def async(func):
     def wrapper(*args, **kwargs):
         return AsyncJob(func, args, kwargs)
-    
     return wrapper
+
+def async_with_input(**async_kwargs):
+    if len(async_kwargs) > 1:
+        raise NotImplemented('@async_with_input currently supports a single input source')
+    
+    if len(async_kwargs) == 0:
+        async_kwargs = None
+    
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return AsyncJob(func, args, kwargs, async_kwargs)
+        return wrapper
+    return decorator
+
+class AsyncInput(object):
+    def set_channel(self, channel):
+        self.channel = channel
+    
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        self.channel.send(('pull', None))
+        t, v = self.channel.receive()
+        if t == 'next':
+            return v
+        else:
+            raise v
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from asyncgen import async
+from asyncgen import async, async_with_input
 
 class SimpleCallsTestCase(unittest.TestCase):
     """
@@ -27,6 +27,46 @@ class SimpleCallsTestCase(unittest.TestCase):
             yield i+j
         
         self.failUnlessEqual(list(f('[', j=']')), ['[]'])
+
+class WithSingleInputTestCase(unittest.TestCase):
+    """
+    Test async calls that accept one input
+    """
+    def test_one_function(self):
+        @async_with_input(i=[1,2,3])
+        def f(i):
+            return (v*v for v in i)
+        
+        self.failUnlessEqual(list(f()), [1, 4, 9])
+    
+    def test_one_function_with_generator(self):
+        @async_with_input(i=(j for j in [1,2,3]))
+        def f(i):
+            return (v*v for v in i)
+        
+        self.failUnlessEqual(list(f()), [1, 4, 9])
+    
+    def test_function_chain(self):
+        @async
+        def g():
+            return (j*j for j in [1,2,3])
+        
+        @async_with_input(i=g())
+        def f(i):
+            return (v*v for v in i)
+        
+        self.failUnlessEqual(list(f()), [1, 16, 81])
+    
+    def test_exception_passing(self):
+        @async
+        def f():
+            raise ValueError('blah')
+        
+        try:
+            f().next() # TODO: should not require .next() here!
+            self.fail('did not raise exception')
+        except ValueError, e:
+            self.failUnless('blah' in str(e))
 
 if __name__ == '__main__':
     unittest.main()
